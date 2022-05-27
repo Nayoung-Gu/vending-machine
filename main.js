39aa1789b88ad7450f5cec489195a61a55932acb
Nayoung Gu <skdud000322@naver.com>
2022-05-27 22:06:04 +0900

    Feat : 음료 구매 및 총 금액 계산 기능 추가

--- main.js
+++ main.js
@@ -1,25 +1,34 @@
 // 1. 입금 기능
-// 입금액 input 값 받은 후 '입금 '버튼을 누르면 잔액란으로 값 이동
+// [x] 입금액 input 값 받은 후 '입금 '버튼을 누르면 잔액란으로 값 이동
 
 // 2. 거스름돈 반환
-// '거스름돈 반환' 버튼 누르면 잔액이 소지금란으로 이동
+// [x] '거스름돈 반환' 버튼 누르면 잔액이 소지금란으로 이동
 
 // 3. 음료 구매
-// 3-1. 음료 버튼 누르면 카트로 이동
-// 3-2. 카트의 음료 버튼을 누르면 하나씩 수량 감소
-// 3-2. '획득' 버튼 누르면 카트에 담긴 음료가 획득한 음료칸으로 이동
-// 3-3. (실패) 카트에 담긴 금액이 잔액을 초과하면 alert
-// 3-4. (성공) 기존 잔액 - 구매금액
+// [x] 3-1. 음료 버튼 누르면 카트로 이동
+// [ ] 3-2. 카트의 음료 버튼을 누르면 하나씩 수량 감소
+// [ ] 3-3. 음료 종류당 하나의 li에 카운트 누적
+// [x] 3-4. '획득' 버튼 누르면 카트에 담긴 음료가 획득한 음료칸으로 이동
+// [x] 3-5. (실패) 카트에 담긴 금액이 잔액을 초과하면 alert
+// [x] 3-6. (성공) 기존 잔액 - 구매금액
 
 // 4. 총금액 계산
-// 획득한 음료의 총금액을 구해 하단에 표시
+// [x] 획득한 음료의 총금액을 구해 하단에 표시
 
+// 5. 금액 단위 표시
+// [ ] 세 자리 단위로 콤마 생성
+
+const drinkCont = document.querySelectorAll(".drink-list li");
 const buyCont = document.querySelector(".cont-buy");
 const returnBtn = buyCont.querySelector(".btn-return");
 const depositInput = buyCont.querySelector(".input-deposit");
 const depositBtn = buyCont.querySelector(".btn-deposit");
 const balance = buyCont.querySelector(".txt-balance");
 const myMoney = document.querySelector(".txt-money");
+const getBtn = document.querySelector(".btn-get");
+
+const selectedColaCont = document.querySelector(".list-staged");
+const boughtColaCont = document.querySelector(".list-myItems");
 
 function getChange() {
   myMoney.textContent =
@@ -37,3 +46,38 @@ function deposit() {
 
 returnBtn.addEventListener("click", getChange);
 depositBtn.addEventListener("click", deposit);
+
+drinkCont.forEach((item) => {
+  item.addEventListener("click", () => {
+    const colaName = item.children[0].children[1].innerText;
+    const selectedCola = document.createElement("li");
+    const selectedColaImg = document.createElement("img");
+    const selectedColaName = document.createElement("strong");
+    const selectedColaCount = document.createElement("span");
+
+    selectedColaImg.setAttribute("src", `./src/images/${colaName}.png`);
+    selectedColaName.innerText = colaName;
+    selectedColaCount.innerText = 1;
+    selectedCola.append(selectedColaImg);
+    selectedCola.append(selectedColaName);
+    selectedCola.append(selectedColaCount);
+    selectedColaCont.append(selectedCola);
+
+    getBtn.addEventListener("click", () => {
+      let price = selectedColaCont.childElementCount * 1000;
+      const totalPriceTxt = document.querySelector(".price-total");
+      console.log(price);
+      if (parseInt(balance.innerText) < price) {
+        alert("잔액이 부족합니다.");
+      } else {
+        balance.innerText = parseInt(balance.innerText) - price + " 원";
+        selectedCola.remove();
+        // price = 0;
+
+        boughtColaCont.append(selectedCola);
+
+        totalPriceTxt.textContent = boughtColaCont.childElementCount * 1000;
+      }
+    });
+  });
+});
