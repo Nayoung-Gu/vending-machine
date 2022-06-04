36618dbbaad4bd35963b094eee80b30e6dc91aee
Nayoung-Gu <skdud000322@naver.com>
2022-06-04 16:03:02 +0900

    Fix : 획득 누적 아이템 표시 기능 및 총 금액 계산 수정

--- main.js
+++ main.js
@@ -1,23 +1,3 @@
-// 1. 입금 기능
-// [x] 입금액 input 값 받은 후 '입금 '버튼을 누르면 잔액란으로 값 이동
-
-// 2. 거스름돈 반환
-// [x] '거스름돈 반환' 버튼 누르면 잔액이 소지금란으로 이동
-
-// 3. 음료 구매
-// [x] 3-1. 음료 버튼 누르면 카트로 이동
-// [x] 3-2. 카트의 음료 버튼을 누르면 하나씩 수량 감소
-// [x] 3-3. 음료 종류당 하나의 li에 카운트 누적
-// [x] 3-4. '획득' 버튼 누르면 카트에 담긴 음료가 획득한 음료칸으로 이동
-// [x] 3-5. (실패) 카트에 담긴 금액이 잔액을 초과하면 alert
-// [x] 3-6. (성공) 기존 잔액 - 구매금액
-
-// 4. 총금액 계산
-// [x] 획득한 음료의 총금액을 구해 하단에 표시
-
-// 5. 금액 단위 표시
-// [ ] 세 자리 단위로 콤마 생성
-
 const vendingMachine = document.querySelector(".vending-machine");
 const drinkCont = vendingMachine.querySelectorAll(".drink-list li");
 const buyCont = vendingMachine.querySelector(".cont-buy");
@@ -106,7 +86,7 @@ selectedColaCont.addEventListener("click", (e) => {
     const selectedCola = e.target.classList[1];
     colaObj[selectedCola] -= 1;
     selectedColaCont.children[0].children[2].innerText -= 1;
-
+    itemCount -= 1;
     if (colaObj[selectedCola] === 0) {
       selectedColaCont.children[0].remove();
     }
@@ -129,14 +109,11 @@ getBtn.addEventListener("click", () => {
       balance.innerText =
         parseInt(balance.innerText) - itemCount * 1000 + " 원";
 
-      console.log(`잔액 : ${parseInt(balance.innerText)}`);
-      console.log(`금액 : ${itemCount * 1000}`);
-      boughtColaCont.innerHTML = selectedColaCont.innerHTML;
+      boughtColaCont.innerHTML += selectedColaCont.innerHTML;
+      totalPriceTxt.textContent = +totalPriceTxt.textContent + itemCount * 1000;
       itemCount = 0;
       selectedColaCont.innerHTML = "";
       colaObj = {};
-
-      totalPriceTxt.textContent = itemCount * 1000;
     }
   }
 });
